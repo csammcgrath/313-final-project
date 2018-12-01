c060438bcd8b4d4f4a7d0a2fcb6d777eaf4c14f0
Sam McGrath <csammcgrath@gmail.com>
2018-12-01 14:48:37 -0700

    Working on homepage

--- public/js/chat.js
+++ public/js/chat.js
@@ -28,7 +28,7 @@ $(document).ready(() => {
                 <li class="left clearfix">\
                     <div class="chat-body clearfix">\
                         <div class="header">\
-                            <b>Anonymous</b><br>\
+                            <b>${data.username}</b><br>\
                         </div>\
                         <p>\
                             ${data.message}\
