2e3670641824f6605da8ab650d50b7825c41845f
Sam McGrath <csammcgrath@gmail.com>
2018-12-04 11:00:26 -0700

    Fixing chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,27 +17,28 @@ $(document).ready(() => {
 
         console.log(`Message: `, msg, ` Type: ${typeof msg}`);
         console.log(`Username: `, usr, ` Type: ${typeof usr}`);
-        console.log(`JSON: `, JSON.stringify({ message: msg, username: usr }));
-        socket.emit('new_message', { message: msg, username: usr });
+        console.log(`JSON: `, );
+        socket.emit('new_message', JSON.stringify({ message: msg, username: usr }));
         $('#userInput').val('');
         // $("#scrollbar").scrollTop($("#scrollbar")[0].scrollHeight);
         return false;
     });
 
-    socket.on('new_message', (message, username) => {
+    socket.on('new_message', (obj) => {
         // let username = $('#usernameWhole').html();
         // let username = $('#userNameChat').val();
-        console.log(`Data obj: `, message, username);
+        let data = JSON.parse(obj);
+        console.log(`Data obj: `, data.message, data.username);
 
-        if (message !== '' && username) {
+        if (data.message !== '' && data.username) {
             $('#chatroom').append(`\
                 <li class="left clearfix">\
                     <div class="chat-body clearfix">\
                         <div class="header">\
-                            <b>${username}</b><br>\
+                            <b>${data.username}</b><br>\
                         </div>\
                         <p>\
-                            ${message}\
+                            ${data.message}\
                         </p>\
                     </div>\
                 </li >`
