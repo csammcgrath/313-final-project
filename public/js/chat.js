e2fe43f2ad3dea10326019e315f2900579715d5c
Sam McGrath <csammcgrath@gmail.com>
2018-12-04 10:37:59 -0700

    Fixing chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,7 +12,7 @@ $(document).ready(() => {
     $('#chatButton').click((e) => {
         e.preventDefault();
 
-        socket.emit('new_message', { message: $('#userInput').val() });
+        socket.emit('new_message', { message: $('#userInput').val(), user: $('#userNameChat').val() });
         $('#userInput').val('');
         // $("#scrollbar").scrollTop($("#scrollbar")[0].scrollHeight);
         return false;
@@ -20,15 +20,15 @@ $(document).ready(() => {
 
     socket.on('new_message', (data) => {
         // let username = $('#usernameWhole').html();
-        let username = $('#userNameChat').val();
-        console.log(`Username: ${username}`);
+        // let username = $('#userNameChat').val();
+        console.log(`Data obj: ${data}`);
 
-        if (data.message !== '' && username) {
+        if (data.message !== '' && data.username) {
             $('#chatroom').append(`\
                 <li class="left clearfix">\
                     <div class="chat-body clearfix">\
                         <div class="header">\
-                            <b>${username}</b><br>\
+                            <b>${data.username}</b><br>\
                         </div>\
                         <p>\
                             ${data.message}\
